9b690f9ec95ac06037eae75dd6af29c59736db2c
Jovan Jaćović <jovan.jacovic@ibis-solutions.com>
2024-07-28 00:14:38 +0200

    Extract method - moving methods getCharge and getFrequentPoints to Movie class from Rental class

--- refactoring/src/main/java/refactoring/Customer.java
+++ refactoring/src/main/java/refactoring/Customer.java
@@ -37,23 +37,18 @@ public class Customer {
 	}
 	
 	public String htmlStatement() {
-		 Enumeration rentals = _rentals.elements();
-		 String result = "<H1>Rentals for <EM>" + getName() + "</EM></ 
-		H1><P>\n";
-		 while (rentals.hasMoreElements()) {
-		 Rental each = (Rental) rentals.nextElement();
-		35
-		 //show figures for each rental
-		 result += each.getMovie().getTitle()+ ": " +
-		 String.valueOf(each.getCharge()) + "<BR>\n";
-		 }
-		 //add footer lines
-		 result += "<P>You owe <EM>" + String.valueOf(getTotalCharge()) + 
-		"</EM><P>\n";
-		 result += "On this rental you earned <EM>" +
-		 String.valueOf(getTotalFrequentRenterPoints()) +
-		 "</EM> frequent renter points<P>";
-		 return result;
+		Enumeration rentals = _rentals.elements();
+		String result = "<H1>Rentals for <EM>" + getName() + "</EM></H1><P>\n";
+		while (rentals.hasMoreElements()) {
+			Rental each = (Rental) rentals.nextElement();
+
+			result += each.getMovie().getTitle() + ": " + String.valueOf(each.getCharge()) + "<BR>\n";
+		}
+
+		result += "<P>You owe <EM>" + String.valueOf(getTotalCharge()) + "</EM><P>\n";
+		result += "On this rental you earned <EM>" + String.valueOf(getTotalFrequentRenterPoints())
+				+ "</EM> frequent renter points<P>";
+		return result;
 	}
 	
 	private double getTotalCharge() {
