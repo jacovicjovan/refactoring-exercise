57b2d3109f43653a54613411c58639b4f3783b9a
Jovan Jaćović <jovan.jacovic@ibis-solutions.com>
2024-07-27 23:24:36 +0200

    Initial example of project that has to be refactored

---
+++ refactoring/src/test/java/refactoring/CustomerTest.java
@@ -0,0 +1,31 @@
+package refactoring;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CustomerTest {
+
+	public void testStatement() {
+		Movie movie1 = new Movie("Regular film", 0);
+		Movie movie2 = new Movie("New Releae film", 1);
+		Movie movie3 = new Movie("Children film", 2);
+		
+		Rental rental1 = new Rental(movie1, 1);
+		Rental rental2 = new Rental(movie2, 2);
+		Rental rental3 = new Rental(movie3, 3);
+		
+		Customer customer = new Customer("John Doe");
+		customer.addRental(rental1);
+		customer.addRental(rental2);
+		customer.addRental(rental3);
+		
+		String statement = customer.statement();
+		String expectedStatement = "Rental Record for John Doe\n";
+		expectedStatement += "\tRegular film	2.0\n";
+		expectedStatement += "\tNew Releae film	6.0\n";
+		expectedStatement += "\tChildren film	1.5\n";
+		expectedStatement += "Amount owed is 9.5\n";
+		expectedStatement += "You earned 4 frequent renter points";
+		
+		assertEquals(statement, expectedStatement);
+	}
+}
