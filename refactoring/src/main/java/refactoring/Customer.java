a783ba5e4b64f2a3f2c9371bb2038090a4c4796c
Jovan Jaćović <jovan.jacovic@ibis-solutions.com>
2024-07-27 23:52:42 +0200

    Removing amountFor method and adding the same logic to Rental class

--- refactoring/src/main/java/refactoring/Customer.java
+++ refactoring/src/main/java/refactoring/Customer.java
@@ -28,7 +28,7 @@ public class Customer {
 			double thisAmount = 0;
 			Rental each = (Rental) rentals.nextElement();
 			// determine amounts for each line
-			thisAmount = amountFor(each);
+			thisAmount = each.getCharge();
 			// add frequent renter points
 			frequentRenterPoints++;
 			// add bonus for a two day new release rental
@@ -44,24 +44,5 @@ public class Customer {
 		return result;
 	}
 	
-	private double amountFor(Rental rental) {
-		double result = 0;
-		switch (rental.getMovie().getPriceCode()) {
-		case Movie.REGULAR:
-			result += 2;
-			if (rental.getDaysRented() > 2)
-				result += (rental.getDaysRented() - 2) * 1.5;
-			break;
-		case Movie.NEW_RELEASE:
-			result += rental.getDaysRented() * 3;
-			break;
-		case Movie.CHILDRENS:
-			result += 1.5;
-			if (rental.getDaysRented() > 3)
-				result += (rental.getDaysRented() - 3) * 1.5;
-			break;
-		}
-		return result;
-	}
 
 }
